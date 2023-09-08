de348450fe362490475f8c41eebe299595a3fe15
ebonatto <ebonatto.dev@gmail.com>
2023-09-08 00:36:41 -0300

    Exercício 4

---
+++ aula01/Exercicios/src/Exercicio04.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Exercicio04 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double salarioMinimo = 1320.00;
+        System.out.printf("Salário Mínimo: R$ %.2f\n", salarioMinimo);
+        System.out.print("Qual seu salário? ");
+        double salarioInput = Double.parseDouble(sc.next());
+        double salarioPerc = salarioInput/salarioMinimo;
+
+        System.out.printf("\nVocê ganha %.2f salários mínimos!", salarioPerc);
+    }
+}
