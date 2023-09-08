7ae0fa512dc7a7af383790b035e48f4c9c785e02
ebonatto <ebonatto.dev@gmail.com>
2023-09-08 12:02:46 -0300

    Exercicio 7 com while

--- aula01/Exercicios/src/Exercicio07.java
+++ aula01/Exercicios/src/Exercicio07.java
@@ -4,10 +4,24 @@ public class Exercicio07 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Informe sua idade: ");
-        int idade = Integer.parseInt(sc.next());
-        System.out.print("Informe seu salário: ");
-        double salario = Double.parseDouble(sc.next());
+        int idade = 0;
+        do {
+            System.out.print("\nInforme sua idade: ");
+            idade = Integer.parseInt(sc.next());
+            if (idade <= 0) {
+                System.out.println("Idade inválida!");
+            }
+        } while (idade <= 0);
+
+        double salario = 0;
+        do {
+            System.out.print("\nInforme seu salário: ");
+            salario = Integer.parseInt(sc.next());
+            if (salario <= 0) {
+                System.out.println("Salário inválido!");
+            }
+        } while (salario <= 0);
+
 
         if (idade >= 18 && salario > 2000.0) {
             System.out.println("Você pode comprar um automóvel!");
