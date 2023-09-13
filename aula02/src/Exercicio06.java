2e6a148032c64d78da38121fae7f811d76933909
ebonatto <ebonatto.dev@gmail.com>
2023-09-13 12:31:00 -0300

    Exercício 6

---
+++ aula02/src/Exercicio06.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Exercicio06 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite uma string: ");
+        String str = sc.nextLine();
+
+        char[] charArr = str.toCharArray();
+        char[] charArrReversed = charArr.clone();
+        int j = 0;
+        for(int i = charArr.length - 1; i >= 0; i--) {
+            charArrReversed[j] = charArr[i];
+            j++;
+        }
+
+        for (char c : charArrReversed) {
+            System.out.print(c);
+        }
+    }
+}
