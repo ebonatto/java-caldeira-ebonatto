e9eedf0f6fed32e2130efd6e924c576f6a84a0fd
ebonatto <ebonatto.dev@gmail.com>
2023-09-13 12:38:27 -0300

    Exercício 7

---
+++ aula02/src/Exercicio07.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Exercicio07 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int guess = 0;
+        System.out.println("Advinhe quantos soldados os espartanos irão enfrentar!");
+        do {
+            System.out.print("Quantos soldados iremos enfrentar? ");
+            guess = sc.nextInt();
+
+            if (guess < 300000) {
+                System.out.println("Um pouco mais!");
+            } else if (guess > 300000){
+                System.out.println("Um pouco menos!");
+            }
+        } while (guess != 300000);
+
+        System.out.println("Isso mesmo! Vamos para guerra!");
+    }
+}
