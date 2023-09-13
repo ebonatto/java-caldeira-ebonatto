9f75d6c9bb3b3baf8c0b8920ddcedc3d84ebb5a9
ebonatto <ebonatto.dev@gmail.com>
2023-09-13 12:10:03 -0300

    Exercício 5

---
+++ aula02/src/Exercicio05.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Exercicio05 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite uma string: ");
+        String str = sc.nextLine();
+
+        int midStr = str.length() / 2;
+
+        String str1 = str.substring(0, midStr);
+        String str2 = str.substring(midStr);
+
+        System.out.printf("Primeira metade da frase:\n%s", str1);
+
+    }
+}
