7e1d1dd0fae1a0f8145e34ea6ec2ad605a95e56d
ebonatto <ebonatto.dev@gmail.com>
2023-09-19 18:25:41 -0300

    Testes no main e construtor com super

--- aula04/Exercicio01/src/Carro.java
+++ aula04/Exercicio01/src/Carro.java
@@ -1,4 +1,8 @@
 public class Carro extends Veiculo {
+    public Carro(String marca, String modelo, int ano) {
+        super(marca, modelo, ano);
+    }
+
     @Override
     public double calcularCustoViagem(int distancia) {
         return 0.20*distancia;
--- aula04/Exercicio01/src/Main.java
+++ aula04/Exercicio01/src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Aula 3 Exercicio 1");
+        System.out.println("Aula 3 Exercicio 1\n");
+
+        Veiculo carro = new Carro("Honda", "Civic", 1998);
+        Veiculo moto = new Moto("Yahama", "MT-03", 2022);
+
+        System.out.printf("Viagem até Capão da Canoa de carro: R$%.2f\n",
+                carro.calcularCustoViagem(138));
+
+        System.out.printf("Viagem até Capão da Canoa de moto: R$%.2f\n",
+                moto.calcularCustoViagem(138));
     }
 }
