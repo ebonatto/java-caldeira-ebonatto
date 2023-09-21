d07354d96c5deb0fe5fdd3df8ce26074dff1405a
ebonatto <ebonatto.dev@gmail.com>
2023-09-21 19:19:08 -0300

    Exercicio 2

---
+++ aula05/src/Exercicio02.java
@@ -0,0 +1,30 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Exercicio02 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Random rand = new Random();
+
+        int[] integers = new int[10];
+
+        for (int i = 0; i < integers.length; i++) {
+            integers[i] = rand.nextInt(101);
+        }
+
+        System.out.printf("Digite um número: ");
+        int guess = sc.nextInt();
+
+        boolean contains = false;
+        for (int n : integers) {
+            if (n == guess) {
+                contains = true;
+            }
+        }
+        if (contains) {
+            System.out.println("Esse número está no Array!");
+        } else {
+            System.out.println("Esse número não está no Array!");
+        }
+    }
+}
