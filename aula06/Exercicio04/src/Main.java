7dae9414f99375d7f1514b0857d416c190ab52d5
ebonatto <ebonatto.dev@gmail.com>
2023-09-27 17:58:40 -0300

    Exercicio 4

---
+++ aula06/Exercicio04/src/Main.java
@@ -0,0 +1,22 @@
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) {
+        LinkedHashMap<String, Double> products = new LinkedHashMap<>();
+
+        products.put("Frango", 16.90);
+        products.put("Arroz", 7.50);
+        products.put("Vassoura", 14.99);
+        
+        printProducts(products);
+
+    }
+
+    public static void printProducts(LinkedHashMap<String, Double> products) {
+        System.out.println("Produtos:");
+        for (Map.Entry<String, Double> product : products.entrySet()) {
+            System.out.printf("%s -> R$ %.2f\n", product.getKey(), product.getValue());
+        }
+    }
+}
