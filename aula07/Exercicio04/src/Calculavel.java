e6c5542f567c415270477adbbce5f62378ebff3f
ebonatto <ebonatto.dev@gmail.com>
2023-10-02 15:08:10 -0300

    Interface Calculavel

---
+++ aula07/Exercicio04/src/Calculavel.java
@@ -0,0 +1,7 @@
+public interface Calculavel {
+    public double adicionar(Numero n);
+    public double subtrair(Numero n);
+    public double multiplicar(Numero n);
+    public double dividir(Numero n);
+    public double operacao(Operacoes op, Numero n);
+}
