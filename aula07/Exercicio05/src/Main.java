8f04a91cd5c2c53370fda63f12bb28c115a9b40f
ebonatto <ebonatto.dev@gmail.com>
2023-10-03 11:08:51 -0300

    Testes main

--- aula07/Exercicio05/src/Main.java
+++ aula07/Exercicio05/src/Main.java
@@ -1,5 +1,25 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+        int age = -1;
+
+        while (age < 0 || age > 150) {
+            try {
+                System.out.print("Qual sua idade? ");
+                age = sc.nextInt();
+                if (age < 0 || age > 150) {
+                    throw new IdadeInvalidaException("Idade inválida! Digite uma idade maior que 0 e menor que 150.");
+                }
+            } catch (InputMismatchException ime) {
+                sc.nextLine();
+                System.out.println("Digite um número inteiro!");
+            } catch (IdadeInvalidaException iie) {
+                System.out.println(iie.getMessage());
+            }
+        }
+        System.out.printf("Parabéns pelos %d anos de vida!\n", age);
     }
 }
