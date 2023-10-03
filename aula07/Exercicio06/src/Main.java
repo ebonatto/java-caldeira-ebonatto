033122f23d220032fa84ec49d6a83847815cb116
ebonatto <ebonatto.dev@gmail.com>
2023-10-03 11:22:00 -0300

    Testes no main

--- aula07/Exercicio06/src/Main.java
+++ aula07/Exercicio06/src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ContaBancaria account = new ContaBancaria("Eduardo", 777.7);
+        try {
+            System.out.println(account.getBalance());
+            account.sacar(111.1);
+            System.out.println(account.getBalance());
+            account.sacar(1000);
+        } catch (SaldoInsuficienteException sie) {
+            System.out.println(sie.getMessage());
+        }
     }
 }
