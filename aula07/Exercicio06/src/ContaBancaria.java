75c1dc91f8daed5ec8945bd7915def0c4c4a9543
ebonatto <ebonatto.dev@gmail.com>
2023-10-03 11:17:39 -0300

    Classe ContaBancaria criada

---
+++ aula07/Exercicio06/src/ContaBancaria.java
@@ -0,0 +1,33 @@
+public class ContaBancaria {
+    private String name;
+    private double balance;
+
+    public ContaBancaria(String name, double balance) {
+        this.name = name;
+        this.balance = balance;
+    }
+
+    public void sacar(double valor) throws SaldoInsuficienteException{
+        if (balance < valor) {
+            throw new SaldoInsuficienteException("Saldo insuficiente!");
+        } else {
+            balance -= valor;
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double setBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+}
