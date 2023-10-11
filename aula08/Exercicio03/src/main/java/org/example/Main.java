8bda6d03e07214545e91b63aff3ea042e8405a54
ebonatto <ebonatto.dev@gmail.com>
2023-10-10 22:15:23 -0300

    Classe calculadora

---
+++ aula08/Exercicio03/src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        Calculadora calc = new Calculadora();
+        System.out.println(calc.potencia(2,4));
+        System.out.println(888.8 + (-111.1));
+        System.out.println(calc.adicao(888.8, -111.1));
+    }
+}
