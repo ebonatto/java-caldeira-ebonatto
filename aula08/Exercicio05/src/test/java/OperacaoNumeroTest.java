db2203f7b013cae46c0259a1f7f84644039fc22b
ebonatto <ebonatto.dev@gmail.com>
2023-10-24 10:28:14 -0300

    teste somaNumerosNegativos

--- aula08/Exercicio05/src/test/java/OperacaoNumeroTest.java
+++ aula08/Exercicio05/src/test/java/OperacaoNumeroTest.java
@@ -18,4 +18,19 @@ public class OperacaoNumeroTest {
         Assertions.assertEquals(135, soma2);
         Assertions.assertEquals(4, soma3);
     }
+
+    @Test
+    void somaNumerosNegativos() {
+        OperacaoNumero operacaoNumero = new OperacaoNumero();
+        int[] nPositivos1 = {-1,-3,-5,-7};
+        int soma1 = operacaoNumero.somarNumeros(nPositivos1);
+        int[] nPositivos2 = {-15,-20,-100};
+        int soma2 = operacaoNumero.somarNumeros(nPositivos2);
+        int[] nPositivos3 = {-1,-1,-1,-1};
+        int soma3 = operacaoNumero.somarNumeros(nPositivos3);
+
+        Assertions.assertEquals(-16, soma1);
+        Assertions.assertEquals(-135, soma2);
+        Assertions.assertEquals(-4, soma3);
+    }
 }
